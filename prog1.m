558ee57b192e95a576f4f282f3c7152f61f8f1c6
Filipe Teixeira Cardoso <filipetc.bsb@gmail.com>
2017-09-04 08:19:08 -0300

    Atualizando

--- prog1.m
+++ prog1.m
@@ -13,13 +13,13 @@ pf_y = zeros(2,1);
 
 % Obtendo pontos de topo e base da altura da pessoa 1 (altura conhecida)
 title('Marque (nessa ordem) o topo e a base da altura (Pessoa 1 - altura conhecida) e pressione enter'), [p1_x,p1_y] = getpts(figure(1));
-plot(p1_x(1),p1_y(1), 'v', 'LineWidth', 2, 'Color', 'Yellow');
-plot(p1_x(2),p1_y(2), '^', 'LineWidth', 2, 'Color', 'Cyan');
+plot(p1_x(1),p1_y(1), 'v', 'LineWidth', 2, 'Color', 'Yellow'); % Topo
+plot(p1_x(2),p1_y(2), '^', 'LineWidth', 2, 'Color', 'Cyan'); % Base
 
 % Obtendo pontos de topo e base da altura da pessoa 2 (altura desconhecida)
 title('Marque (nessa ordem) o topo e a base da altura (Pessoa 2 - altura desconhecida) e pressione enter'), [p2_x,p2_y] = getpts(figure(1));
-plot(p2_x(1),p2_y(1), 'v', 'LineWidth', 2, 'Color', 'Yellow');
-plot(p2_x(2),p2_y(2), '^', 'LineWidth', 2, 'Color', 'Cyan');
+plot(p2_x(1),p2_y(1), 'v', 'LineWidth', 2, 'Color', 'Yellow'); % Topo
+plot(p2_x(2),p2_y(2), '^', 'LineWidth', 2, 'Color', 'Cyan'); % Base
 
 % Obtendo Reta 1 do primeiro par de retas paralelas
 title('Indique um segmento (Reta 1) do primeiro par de paralelas - marque dois pontos e pressione enter'), [r_x,r_y] = getline(figure(1));
@@ -84,15 +84,39 @@ line(r_x,r_y,'LineStyle',':', 'LineWidth', 2, 'Color', 'Magenta');
 
 % Cálculo da reta que passa na base das pessoas a serem comparadas
 % Cálculo dos coeficientes m e n em y = m*x+n
-p_m = (p2_y(2)-p1_y(2))/(p2_x(2)-p1_x(2)); % m = (y2-y1)/(x2-x1)
-p_n = p2_y(2)-p_m*p2_x(2); % n = y1-m*x1 = y2-m*x2
-% Plot da linha
+pb_m = (p2_y(2)-p1_y(2))/(p2_x(2)-p1_x(2)); % m = (y2-y1)/(x2-x1)
+pb_n = p1_y(2)-pb_m*p1_x(2); % n = y1-m*x1 = y2-m*x2
+% Plot da reta
 r_x = [0,img_size(2)];
-r_y = p_m*r_x+p_n;
+r_y = pb_m*r_x+pb_n;
 line(r_x,r_y,'LineStyle',':', 'LineWidth', 2, 'Color', 'Cyan');
 
 % Cálculo e plot do ponto v de cruzamento entre a linha de fuga e reta base
-v_x = (lf_n-p_n)/(p_m-lf_m); % x = (n2-n1)/(m1-m2)
-v_y = p_m*v_x+p_n; % y = m1*x+n1 = m2*x+n2
+v_x = (lf_n-pb_n)/(pb_m-lf_m); % x = (n2-n1)/(m1-m2)
+v_y = pb_m*v_x+pb_n; % y = m1*x+n1 = m2*x+n2
 plot(v_x,v_y, 'x', 'LineWidth', 2, 'Color', 'Blue');
 
+% Cálculo e plot do ponto vz acima da pessoa de altura conhecida (p1) para
+% o qual z tende ao infinito
+vz_x = p1_x(1);
+vz_y = 1;
+plot(vz_x,vz_y, 'x', 'LineWidth', 2, 'Color', 'Black');
+
+% Cálculo da reta pt que passa no topo da pessoa de altura desconhecida (p2)
+% e pelo ponto v
+% Cálculo dos coeficientes m e n em y = m*x+n
+pt_m = (v_y-p2_y(1))/(v_x-p2_x(1)); % m = (y2-y1)/(x2-x1)
+pt_n = v_y-pt_m*v_x; % n = y1-m*x1 = y2-m*x2
+% Plot da reta
+r_x = [0,img_size(2)];
+r_y = pt_m*r_x+pt_n;
+line(r_x,r_y,'LineStyle',':', 'LineWidth', 2, 'Color', 'White');
+
+% Cálculo e plot do ponto p21 pertencente à reta pt logo acima da pessoa de
+% altura conhecida
+p21_x = p1_x(1);
+p21_y = pt_m*p21_x+pt_n;
+plot(p21_x,p21_y, 'v', 'LineWidth', 2, 'Color', 'Yellow');
+
+%
+H = (1.70)*(((norm([p21_x,p21_y]-[p1_x(2),p1_y(2)]))*(norm([vz_x,vz_y]-[p1_x(1),p1_y(1)])))/(((norm([p1_x(1),p1_y(1)]-[p1_x(2),p1_y(2)]))*(norm([vz_x,vz_y]-[p21_x,p21_y])))));
